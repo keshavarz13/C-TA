54d348adfa0eb895ce934ce3d277500a2cd61ebb
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-11-22 18:12:52 +0330

    add new presentation

---
+++ presentation/teaching 7/Codes.c
@@ -0,0 +1,71 @@
+//-----------------------------------------
+// arrays :) 
+//-----------------------------------------
+// #include <stdio.h>
+// void main(void){
+// 	int n;
+// 	printf("Enter n: ", n);
+// 	scanf("%d", &n);
+// 	int number[n];
+// 	double average;
+// 	int sum, large_size, small_size, i;
+// 	sum = large_size = small_size = 0;
+// 	for(i = 0; i < n; i++)
+// 		scanf("%d", &(number[i]));
+
+// 	for(i = 0; i < n; i++)
+// 		sum += number[i];
+// 	average = (1.0 * sum) / n;
+// 	for(i = 0; i < n; i++)
+// 		if(number[i] >= average)
+// 			large_size++;
+// 		else
+// 			small_size++;
+// 	printf("average = %f\n", average);
+// 	printf("Small Size = %d, Larg Size = %d\n", small_size, large_size);
+// }
+
+
+
+//-----------------------------------------
+// advanced loop :) (first point)
+//-----------------------------------------
+// #include <stdio.h>
+// void main(void){
+//     int i, j ;
+//     for(i = 0; i < 10; printf("slm : "), printf("%d\n",i) ,  i++) ;
+// }
+
+
+
+
+//-----------------------------------------
+// advanced loop :) (nested loop)
+//-----------------------------------------
+// #include <stdio.h>
+// void main(void){
+//     int n, i, j;
+// 	printf("Enter n: ", n);
+// 	scanf("%d", &n);
+//     for(i= 1; i <= n; i++){
+// 		for(j = 0; j < i-1; j++)
+// 			printf(" ");
+
+// 		for(j = 1; j <= i; j++)
+// 			printf("*");
+
+// 		printf("\n");
+// 	}
+
+// 	for(i= n-1; i >= 1; i--){
+// 		for(j = 1; j < i; j++)
+// 			printf(" ");
+// 		for(j = 1; j <= i; j++)
+// 			printf("*");
+// 		printf("\n");
+// 	}
+// }
+	
+
+
+
